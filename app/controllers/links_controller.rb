8108c4bd3ce351a8826911b1e8fb10567289e264
Ghazaly <gazaly@gmail.com>
2014-02-17 14:35:10 +0800

    Added generate random string method to Links controller.

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -4,9 +4,13 @@ class LinksController < ApplicationController
 		redirect_to action:'new'
 	end
 
+	def new
+		@link = Link.new
+	end
+
 	def create
 		@link = Link.new(post_params)
-		@link.shortened_url = "meme"
+		@link.shortened_url = generate_random_string
 		@link.save
 		redirect_to action:'new'
 	end
@@ -17,6 +21,12 @@ class LinksController < ApplicationController
 	end
 
 	private 
+
+		def generate_random_string(size = 5)
+			charset = ('a'..'z').to_a + (0..9).to_a
+			(0...size).map{ charset.to_a[rand(charset.size)] }.join
+		end
+
 		def post_params
 			params.require(:link).permit(:original_url)
 		end
