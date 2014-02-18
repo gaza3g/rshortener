4bd9a5cc61362109c052519ae37a294162bafc8d
Ghazaly <gazaly@gmail.com>
2014-02-18 10:40:20 +0800

    Use alert instead of label to diplay shortened url. Added vistiors link at the top right corner of the page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,2 @@
 module ApplicationHelper
-	def flash_class(level)
-	    case level
-	        when :notice then "label label-info"
-	        when :success then "label label-success"
-	        when :error then "label label-error"
-	        when :alert then "label label-error"
-	    end
-	end
 end
